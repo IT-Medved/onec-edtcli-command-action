3b3a5c4480dc84f91a1b042a49c4fa7c110db7dd
IT-Medved <it.medve.dev@yandex.com>
2023-08-25 01:28:09 +0300

    add commands for export and import

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import * as core from '@actions/core'
-import { wait } from './wait'
+
+import { exec } from '@actions/exec'
+import path from 'path'
 
 /**
  * The main function for the action.
@@ -7,18 +9,40 @@ import { wait } from './wait'
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    const exportParam = core.getBooleanInput('export')
+    const importParam = core.getBooleanInput('import')
+    const from = core.getInput('from')
+    const to = core.getInput('to')
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    if (exportParam && importParam) {
+      throw new Error('export and import options cannot be used together')
+    }
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    if (exportParam) {
+      exec('1cedtcli.sh', [
+        '--data',
+        '/tmp/ws',
+        '-command',
+        'export',
+        '--project',
+        path.resolve(from),
+        '--configuration-files',
+        path.resolve(to)
+      ])
+    }
+
+    if (importParam) {
+      exec('1cedtcli', [
+        'export',
+        '--project',
+        'D:project-1',
+        '--configuration-files',
+        'D:XML-1'
+      ])
+    }
 
     // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    //core.setOutput('time', new Date().toTimeString())
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
